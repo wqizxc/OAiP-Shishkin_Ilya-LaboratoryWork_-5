4deb4ac094a1cd6dc78291a241eec685cf678f4e
wqizxcas <hopellesss@rambler.ru>
2026-01-22 14:46:58 +0300

    Добавил первый проект

---
+++ Shishkin_LR5/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Shishkin_LR5;
+
+HoursToMinute converter = new HoursToMinute();
+int h, m;
+converter.Read(out h, out m);
+int total = converter.Minutes(h, m);
+converter.Display(h, m, total);
